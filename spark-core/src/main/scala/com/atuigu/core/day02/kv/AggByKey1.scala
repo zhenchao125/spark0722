1862fadaba94af59c65202eaad35e398f3011090
John Doe <30752740@qq.com>
2019-12-09 15:58:05 +0800

    更新

--- spark-core/src/main/scala/com/atuigu/core/day02/kv/AggByKey1.scala
+++ spark-core/src/main/scala/com/atuigu/core/day02/kv/AggByKey1.scala
@@ -1,6 +1,5 @@
 package com.atuigu.core.day02.kv
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -11,8 +10,20 @@ object AggByKey1 {
     def main(args: Array[String]): Unit = {
         val conf: SparkConf = new SparkConf().setAppName("AggByKey").setMaster("local[2]")
         val sc: SparkContext = new SparkContext(conf)
-        val rdd = sc.parallelize(List(("a",3),("a",2),("c",4),("b",3),("c",6),("c",8)),2)
+        val rdd = sc.parallelize(List(("a", 3), ("a", 2), ("c", 4), ("b", 3), ("c", 6), ("c", 8)), 2)
+        /*val res = rdd.aggregateByKey((Int.MinValue, Int.MaxValue))(
+            (kv, e) => (kv._1.max(e), kv._2.min(e)),
+            (kv1, kv2) => (kv1._1 + kv2._1, kv1._2 + kv2._2))*/
+        val res = rdd.aggregateByKey((Int.MinValue, Int.MaxValue))(
+            {
+                case ((max, min), e) => (max.max(e), min.min(e))
+            },
+            {
+                case ((max1, min1), (max2, min2)) => (max1 + max2, min1 + min2)
+            }
+        )
         
+        // 计算出来每个key对应的值的平均值!!
         res.collect.foreach(println)
         sc.stop()
         
